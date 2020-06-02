143179b1a7b1fd895376e84c15ccd422f86035f5
Sharif Hasan <me.sharif.hasan@gmail.com>
2020-06-02 19:21:26 +0600

    Bug fix

---
+++ src/me/res/html/keyboard.js
@@ -0,0 +1,10 @@
+/*
+$("#keyboard-up").click(function(e){
+	$("#keyboard").show();
+	$("#keyboard-focus").focus();
+});
+$("#keyboard-focus").on('input',function(e){
+	var ch=e.originalEvent.data.slice(-1);
+	holder.ws_send("keyboard,$"+ch+"$");
+});
+*/
